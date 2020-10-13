c056ea35b678a61cb4d3125af6830fa93b2d4b51
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2020-10-13 17:30:45 +0400

    constant dummy data

---
+++ constants/index.js
@@ -0,0 +1,14 @@
+export const NAV_ITEMS = [
+  {
+    name: "Posts",
+    isPublic: false
+  },
+  {
+    name: "Users",
+    isPublic: false
+  },
+  {
+    name: "About & License",
+    isPublic: true
+  },
+];
