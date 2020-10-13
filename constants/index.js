12a6d532a472030a02e36ee5d1ac828388401983
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2020-10-13 21:50:59 +0400

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,14 +1,17 @@
 export const NAV_ITEMS = [
   {
     name: "Posts",
+    metaUrl: "/posts",
     isPublic: false
   },
   {
     name: "Users",
+    metaUrl: "/users",
     isPublic: false
   },
   {
     name: "About & License",
+    metaUrl: "/info",
     isPublic: true
   },
 ];
