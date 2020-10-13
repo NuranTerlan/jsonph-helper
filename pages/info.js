e76913d594b0289c41da2c610850d8b3a21be898
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2020-10-13 21:52:47 +0400

    Create info.js

---
+++ pages/info.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+
+import styles from "../styles/Info.module.scss";
+
+import LayoutBuilder from "../components/layouts/layout";
+import FormBtn from "../components/common/form/button";
+
+const InfoPage = () => {
+    let router = useRouter();
+
+    return (
+        <div className={styles.appContainer}>
+            <Head>
+                <title>JSONplaceholder &bull; UI &bull; About & License</title>
+            </Head>
+
+            <LayoutBuilder className={styles.page}>
+                <h1>About & License</h1>
+                <FormBtn onClick={() => router.back()}>Back</FormBtn>
+            </LayoutBuilder>
+        </div>
+    );
+}
+
+export default InfoPage;
