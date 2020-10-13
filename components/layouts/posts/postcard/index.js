275ef84ff32c6f62a2a5522f05a79faa8f03712a
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2020-10-14 02:28:43 +0400

    Update index.js

--- components/layouts/posts/postcard/index.js
+++ components/layouts/posts/postcard/index.js
@@ -1,15 +1,18 @@
 import React from "react";
+import Link from "next/link";
 import cn from "classnames";
 
 import styles from "./postcard.module.scss";
 
 
-const PostCard = ({ num, title, className }) => {
+const PostCard = ({ link = "#", num, title, className }) => {
     return (
-        <div className={cn(styles.card, className)}>
-            <h2 className={styles.title}>{title}</h2>
-            <span className={styles.order}>{num}</span>
-        </div>
+        <Link href={link}>
+            <div className={cn(styles.card, className)}>
+                <h2 className={styles.title}>{title}</h2>
+                <span className={styles.order}>{num}</span>
+            </div>
+        </Link>
     );
 };
 
