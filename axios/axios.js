d3d74796c24187571c94104631c4c5acafd5978f
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2020-10-14 19:16:12 +0400

    Update axios.js

--- axios/axios.js
+++ axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://my-json-server.typicode.com/NuranTerlan/blog-data",
+    baseURL: "https://my-json-server.typicode.com/NuranTerlan/blog-data",
     headers: {
         headerType: "example header type"
     }
