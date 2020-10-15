0eba49bcbd067059d7795cde0bc737eb0efb45c2
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2020-10-16 00:38:56 +0400

    Update requestEndpoints.js

--- axios/requestEndpoints.js
+++ axios/requestEndpoints.js
@@ -1,2 +1,5 @@
 // BASE_URL -> jsonplaceholder.typicode.com + /posts request endpoint for fetching data from api
-export const postsRequest = "/posts";
+export const getPostsRequest = "/posts";
+export const getPostRequest = (id) => {
+    return `${getPostsRequest}/${id}`;
+};
