7fcd5bc8be30a60a679443f7a52aaa15aee1a481
Nuran Terlan <nuran.terlanli@asoiu.edu.az>
2020-10-18 12:38:58 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useEffect } from "react";
 import { connect } from "react-redux";
 import Router from "next/router";
@@ -10,7 +11,13 @@ const FirstPage = ({ isLoggedIn }) => {
             else
                 Router.push("/posts")
     });
-    return null;
+    return (
+        <Head>
+            <title>Welcome &bull; JSONplaceholder &bull; UI</title>
+            <meta name="description" content="JSONPlaceholder Helper Project created for learning to use Redux state management tool in NextJs project. Redux with NextJs, React, Global State. Context in NextJs. Global Context in React. Created by DEVVHALE. Nuran Terlan 2020. devvhale. nuran terlan" />
+            <meta name="google-site-verification" content="iKC2hErCRhHDyAcM9g93KcaEYgPDTORTKjsCf6nzYas" />
+        </Head>
+    );
 };
 
 const mapStateToProps = (state) => {
